67d2145f1d0e15446d521afe3ec723067eae6cda
Juan Sebastián Mendoza <cebo_052000@hotmail.com>
2023-06-01 12:43:58 -0500

    add server-side session fetching

--- app/components/AuthProvider.tsx
+++ app/components/AuthProvider.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 
-const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+interface AuthProviderProps {
+  session: Session | null;
+  children: React.ReactNode;
+}
+
+const AuthProvider = ({ session, children }: AuthProviderProps) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,35 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import AuthProvider from '@/app/components/AuthProvider';
-import './globals.css';
+import { Metadata } from 'next';
+import { Session, getServerSession } from 'next-auth';
 import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'To-do App',
   description:
     'A simple to-do app built with Next.js, TypeScript, GraphQL, MongoDB, and NextAuth.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  let session: Session | null;
+
+  try {
+    session = await getServerSession(authOptions);
+  } catch {
+    session = null;
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
+        <AuthProvider session={session}>
           <main>{children}</main>
         </AuthProvider>
       </body>
