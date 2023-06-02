8b837f6141618a678aef788647dd433191f5194f
Juan Sebastián Mendoza <cebo_052000@hotmail.com>
2023-06-01 19:20:09 -0500

    add page protection with middleware

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,15 +1,23 @@
-// import { useSession } from 'next-auth/react';
-import Link from 'next/link';
+'use client';
 
-export default function Todos() {
-  // const { data } = useSession();
+import { Metadata } from 'next';
+import { useSession } from 'next-auth/react';
+
+export const metadata: Metadata = {
+  title: 'Your To-dos',
+};
+
+export default function TodosPage() {
+  const { data: session } = useSession();
 
   return (
-    <section>
-      <h1>These are all of your to-dos:</h1>
-      <Link href="/" className="btn-primary">
-        Go home
-      </Link>
+    <section className="min-h-screen px-8 pt-20">
+      <header>
+        <h1>
+          Hey, {session?.user?.name ?? 'user'} 👋!
+          These are all of your to-dos:
+        </h1>
+      </header>
     </section>
   );
 }
